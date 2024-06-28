a019c73157573af2eb06e1181c2b2bdca7752f2c
Maria <emailtomariageorge@gmail.com>
2024-06-27 19:22:36 -0700

    4-4

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 def main():
 
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    print(*numbers)
-    print(maxval, minval)
+    maxval = 0
+    for num in range(5): 
+        number = int(input('Enter your number: '))
+        numbers.append(number)
+    minval = numbers[0]
+    for x in numbers:
+        if x > maxval:
+            maxval = x
+    for x in numbers:
+        if x < minval:
+            minval = x
+    print(numbers)
+    print('Max Number: ')
+    print(maxval)
+    print('Smallest Number: ')
+    print(minval)
     ########################################
     # Do not delete the return statement
     ########################################
